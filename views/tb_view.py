f3d1e519ef502863ce97aa990923d7d4fa1b4ca1
Reese Anwyl Ramos <seer@192.168.1.5>
2025-07-26 19:32:34 +0800

    renamed results html file

--- views/tb_view.py
+++ views/tb_view.py
@@ -4,3 +4,7 @@ class TBView:
     @staticmethod
     def render_medform_list(medforms):
         return render_template('tb_list.html', medforms=medforms)
+    
+    @staticmethod
+    def render_medical_form_result(tb_result, tb_probability):
+        return render_template('results.html', result=tb_result, probability=tb_probability)
